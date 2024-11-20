06efa7ee3977f72dbb023582e334c0d649999eec
jt-max-coder <jte321jr@gmail.com>
2024-11-20 11:24:49 -0600

    added a lot to endPanel
    mostly finished endPanel, still need to make the test coordinates scaleable and add a option for the player to restart but other than that its done

--- SplendorMain/src/EndPanel.java
+++ SplendorMain/src/EndPanel.java
@@ -2,36 +2,46 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.TreeSet;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class EndPanel extends JPanel implements MouseListener{
     private int startX;//= getWidth()/2;
     private int startY;// = getHeight()/5;
     private TreeSet<Player> scores;
-
+    private BufferedImage bkg;
     //constructor
     public EndPanel(TreeSet<Player> s){
         scores = s;
-        startX = 800;
-        startY = 100;
+        startX = 750;
+        startY = 475;
         addMouseListener(this);
+        try {
+            bkg = ImageIO.read(EndPanel.class.getResource("/images/endBackground.jpg"));
+        } 
+        catch (IOException ex) {
+            System.out.println("backround is OVER");
+        }
     }
 
     //main paint method, need to add scores to be dysplayed aswell but it is mostly done
     public void paint(Graphics g){
         super.paint(g);
+        g.drawImage(bkg, 0, 0, getWidth(), getHeight(), null); //background
+
         Iterator<Player> iter = scores.iterator();
         Player winner = iter.next();
-        g.setFont(new Font("Times New Roman", Font.PLAIN, 44));
-        g.drawString(winner.getName() + " has won!", startX, startY);
+        g.setFont(new Font("Times New Roman", Font.PLAIN, 50));
+        g.drawString(winner.getName() + "! (score, " + winner.getTotalVP() + ")", startX, startY);
 
-        g.setFont(new Font("Times New Roman", Font.PLAIN, 22));
-        for(int i = 0; i < scores.size()-1; i++)
-        {
+        g.setFont(new Font("Times New Roman", Font.PLAIN, 50));
+        for(int i = 0; i < scores.size()-1; i++){
             Player p = iter.next();
-            g.drawString(p.getName() + " has lost :(", (startX-500) + i*500 , (startY +200 ));
+            g.drawString(p.getName() + "(score, " + p.getTotalVP() + ")", (startX-700) + i*750 , (startY +250 ));
         }
         //repaint();
     }
