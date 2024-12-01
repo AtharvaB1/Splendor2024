4a611484bcf140d61207f7666ba49106e058aff1
AtharvaB1 <atharvabisht11@gmail.com>
2024-12-01 13:51:06 -0600

    centered and fixed img bugs

--- SplendorMain/src/Card.java
+++ SplendorMain/src/Card.java
@@ -16,9 +16,9 @@ public class Card{
     public Card(String input, int t){
         cost = new HashMap<String,Integer>();
         try {
-            img = ImageIO.read(Card.class.getResource("src\\Images\\Card Images\\" + input+ ".png")); //imgs broken
+            img = ImageIO.read(Card.class.getResource("Images\\Card Images\\" +input+ ".png")); //imgs broken
         } catch (Exception e) {
-            System.out.println(e+"src\\Images\\Card Images\\" + input+ ".png");
+            System.out.println(e+"Images\\Card Images\\" + input+ ".png");
             return;
         }
         String[] vars = input.split(" ");
