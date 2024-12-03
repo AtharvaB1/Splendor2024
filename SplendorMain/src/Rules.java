ce56559efa1913275b7aae743554d116004a23ae
AtharvaB1 <atharvabisht11@gmail.com>
2024-12-03 00:50:09 -0600

    merge fix

---
+++ SplendorMain/src/Rules.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Rules extends JFrame {
+    JFrame popFrame;
+    
+    public Rules(){
+        popFrame = new JFrame("Rules");
+        popFrame.setDefaultCloseOperation(3);
+        
+        popFrame.setBounds(getWidth()/2, getHeight()/2, 200, 200);
+
+    }
+
+}
